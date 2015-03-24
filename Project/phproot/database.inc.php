c1cd84efb286d6754ddedf0e7a82ad1f36677058
freedrikp <fredrik40@msn.com>
2015-03-24 19:21:52 +0100

    Börjat med klient

--- Project/phproot/database.inc.php
+++ Project/phproot/database.inc.php
@@ -101,118 +101,11 @@ class Database {
 		}
 		return $count;
 	}
-	
-	/**
-	 * Check if a user with the specified user id exists in the database.
-	 * Queries the Users database table.
-	 *
-	 * @param userId The user id 
-	 * @return true if the user exists, false otherwise.
-	 */
-	public function userExists($userId) {
-		$sql = "select uName from Users where uName = ?";
-		$result = $this->executeQuery($sql, array($userId));
-		return count($result) == 1; 
-	}
 
 	/*
 	 * *** Add functions ***
 	 */
 
-	public function getMovieNames(){
-		$sql = "select mName from Movies";
-		$results = $this->executeQuery($sql);
-		
-		foreach($results as $result){
-			$movienames[] = $result['mName'];
-		}
-		return $movienames; 
-	}
-
-	public function getDates($movie){
-		$sql = "select sDate from Shows where mName=?";
-		$results = $this->executeQuery($sql, array($movie));
-		
-		foreach($results as $result){
-			$sDates[] = $result['sDate'];
-		}
-		return $sDates; 
-	}
-
-	public function getPerformance($movie, $date){
-		$sql = "select sDate,mName,tName, (capacity-nbrBooked) as freeSeats from Shows natural join Theaters where sDate = ? and mName = ?";
-		$results = $this->executeQuery($sql, array($date, $movie));
-		$c = count($results);
-		if($c == 1){ 
-		 foreach($results as $result){
-		 	$sDate = $result['sDate'];
-		 	$mName = $result['mName'];
-		 	$theater = $result['tName'];
-		 	$freeSeats = $result['freeSeats'];
-		}
-		$show['sDate'] = $sDate;
-		$show['mName'] = $mName;
-		$show['tName'] = $theater;
-		$show['freeSeats'] = $freeSeats;
-		$show['c'] = $c;
-		}else
-		{
-			$show['error'] = 1;
-		}
-		return $show; 
-	}
-
-	public function bookTicket($show, $user){//$movie, $date, $user
-		$sql = "select (capacity-nbrBooked) as freeSeats from Shows natural join Theaters where sDate = ? and mName = ? for update";
-		$this->conn->beginTransaction();		
-//$this->$conn->beginTransaction();
-		$results = $this->executeQuery($sql, array($show['sDate'], $show['mName']));
-		$count = count($results);
-		if ($count == 1){
-			foreach ($results as $result){
-				$freeSeats = $result['freeSeats'];
-			}
-		}else{
-			$this->conn->rollBack();
-			//conn->rollback();
-			return -1;
-		}
-
-		if ($freeSeats <= 0){
-			$this->conn->rollBack();
-			//$conn->rollback();
-			return -2;
-		}
-		$sql = "insert into Reservations(uName,sDate,mName) values(?, ?, ?)";
-		$count = $this->executeUpdate($sql, array($user, $show['sDate'], $show['mName']));
-		if ($count != 1){
-			$this->conn->rollBack();
-			//$conn->rollback();
-			return -3;
-		}
-		$sql = "update Shows set nbrBooked = nbrBooked+1 where mName = ? and sDate = ?";
-		$count = $this->executeUpdate($sql, array($show['mName'], $show['sDate']));
-		if ($count != 1){
-			$this->conn->rollBack();
-			//$conn->rollback();
-			return -4;
-		}
-		$sql = "select last_insert_id() as last_id";
-		$results = $this->executeQuery($sql);
-		$count = count($results);
-		if($count == 1){
-		foreach($results as $result){
-			$rNbr = $result['last_id'];
-		}
-		}else{
-			$this->conn->rollBack();
-			//$conn->rollback();
-			return -5;
-		}
-		$this->conn->commit();
-		//$this->$conn->commit();
-		return $rNbr;
-		
-	}
+	
 }
 ?>
